8dea4bb4c4ce4dbc151f3bee38c5cab2e81a8efa
Vicente <vvcastro@uc.cl>
2023-09-25 16:04:59 +0200

    checking i/o

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 
 std::shared_ptr<Node> node;
+std::string peerAddress("192.168.56.4");
 
 // Exit code and close active instances
 void handleCtrlC(int signal) {
@@ -21,6 +22,19 @@ int main(int argc, char* argv[]) {
     node = std::make_shared<Node>(execArgs[0], execArgs[1]);
     node->showConnectionData();
 
+    std::string userInput;
+    while (true) {
+        std::cout << "Enter a command (or 'exit' to quit).\n> ";
+
+        // capture input from user
+        std::cin >> userInput;
+        if (userInput == "exit") {
+            break;
+        }
+
+        // construct message
+        node->sendMessage(peerAddress, userInput);
+    }
 
     node->closeConnection();
     return 0;
