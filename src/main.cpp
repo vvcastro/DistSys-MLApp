68d08fd283d65f8611061390074b41929dfa60af
Vicente <vvcastro@uc.cl>
2023-09-26 04:56:46 +0200

    ReliableBroadcast

--- src/main.cpp
+++ src/main.cpp
@@ -25,13 +25,11 @@ int main(int argc, char* argv[]) {
     std::cout << "Enter a command: ( start )\n> ";
     std::cin >> userInput;
     if (userInput == std::string("start")) {
-        while (true) {
-            std::string baseLetters("ABCDEFGHIJKLMN");
-            for (char& c : baseLetters) {
-                std::string toSendd(3, c);
-                node->broadcastMessage(toSendd);
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-            }
+        std::string baseLetters("ABCDEFGHIJKLMN");
+        for (char& c : baseLetters) {
+            std::string toSendd(3, c);
+            node->broadcastMessage(toSendd);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
         }
     }
 
